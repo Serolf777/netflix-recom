e796dd639c9d23c089d7342219c317d96497bb34
Serolf777 <kookie-serolf@hotmail.com>
2025-10-31 10:20:01 -0400

    css adjustments for header

--- src/App.js
+++ src/App.js
@@ -36,38 +36,42 @@ function App() {
           Welcome to Netflix recommendations!
         </div>
       </div>
+      <div className="site-body">
+        <h3 className="announcement">These are the top shows on netflix RIGHT NOW!</h3>
 
-      <h3 className="header">These are the top shows on netflix RIGHT NOW!</h3>
-
-      {loading ?
-          <div class="lds-dual-ring"></div>
-          : (
-            showsArray.map((show, showNumber) => {
-                return <NetflixShow key={showNumber} showData={show} showNumber={showNumber}/>
-              }
-            )
-        )
-      }
-      <FormProvider {...{ register, getValues }}>
-        <form name="test-form" className="netflix-form" onSubmit={(e) => e.preventDefault()}>
-          <div className="ai-search-section">
-            <div className="search-header">Would you like to search using AI results?</div>
-            <div className="button-container">
-              <button type="submit" value="submit" onClick={() => onClickHandler()} > 
-                Submit 
-              </button>
+        {loading ?
+            <div class="lds-dual-ring"></div>
+            : (
+              showsArray.map((show, showNumber) => {
+                  return <NetflixShow key={showNumber} showData={show} showNumber={showNumber}/>
+                }
+              )
+          )
+        }
+        <FormProvider {...{ register, getValues }}>
+          <form name="test-form" className="netflix-form" onSubmit={(e) => e.preventDefault()}>
+            <div className="ai-search-section">
+              <div className="search-header">Would you like to search using AI results?</div>
+              <div className="button-container">
+                <button type="submit" value="submit" onClick={() => onClickHandler()} > 
+                  Submit 
+                </button>
+              </div>
             </div>
-          </div>
 
-          <div className="search-header">Would you like to narrow down by genre?</div>
-          <Dropdown genres={genresList} onChangeHandler={onChangeHandler} register={() => register("genres")} />
-          <div className="search-header">Or search by your own keywords:</div>
-          <input type="text" className="searchbar" {...register("searchbar")} />
-          <button type="submit" value="submit" > 
-            Submit 
-          </button>
-        </form>
-      </FormProvider>
+            <div className="search-header">Would you like to narrow down by genre?</div>
+            <Dropdown genres={genresList} onChangeHandler={onChangeHandler} register={() => register("genres")} />
+            <div className="search-header"> 
+              <div className="searchby">Or search by your own keywords: </div>
+              <div className="in-progress">(In progress)</div>
+            </div>
+            <input type="text" className="searchbar" {...register("searchbar")} disabled={true}/>
+            <button type="submit" value="submit" disabled={true}> 
+              Submit 
+            </button>
+          </form>
+        </FormProvider>
+      </div>
 
       <div className="footer">
           <div className="footer-text">
