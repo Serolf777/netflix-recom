6d4875f1ed42aa77fbdb3c9cc7c895530d91cf97
Serolf777 <kookie-serolf@hotmail.com>
2025-12-17 15:32:47 -0500

    initial UI implementations for uma-project

---
+++ src/uma-project/supportCards.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+
+export interface SupportCardsProps {
+    imgUrl: string;
+    alt: string
+};
+
+const SupportCards: FC<SupportCardsProps> = ({ imgUrl, alt }) => {
+    return (
+        <div className="support-card">
+            <img src={imgUrl} alt={alt} height={100} />
+        </div>
+    )
+};
+
+export default SupportCards;
--- src/uma-project/uma-project.tsx
+++ src/uma-project/uma-project.tsx
@@ -6,6 +6,8 @@ import Dropdown from '../shared/dropdown.tsx';
 import { staticUmaList, statTypes } from '../utilities/constants.tsx';
 import { useNavigate } from "react-router";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import SupportCards from './supportCards.tsx';
+import cards from './cards/cards.tsx';
 
 const UmaProject: FC = () => {
     const navigate = useNavigate();
@@ -35,6 +37,9 @@ const UmaProject: FC = () => {
                 <div className="uma-project-header">
                     Uma Project
                 </div>
+                <div>
+                    This will provide suggested cards based on your requested stat requirements.
+                </div>
                 <FormProvider { ...methods }>
                     <form className="uma-project-form">
                         <div>
@@ -57,13 +62,69 @@ const UmaProject: FC = () => {
                                     })
                                 }
                             </div>
-                            <div>
-                                Test Input
+                            <div className="uma-bonuses-container">
+                                <div className="uma-bonuses-header">
+                                    <div className="bonus-header-msg">
+                                        Uma Bonuses
+                                    </div>
+                                    <div className="bonus-header-helper-note">
+                                        The percentages on the uma's stat screen, converted to decimal. 
+                                    </div>
+                                </div>
+                                    <div className="uma-bonuses">
+                                        <label>
+                                            Spd
+                                        </label>
+                                        <input type="number" defaultValue="1.06" min="1.00" max="1.30" step=".01" className="uma-bonus-spd" />
+                                        <label>
+                                            Stam
+                                        </label>
+                                        <input type="number" defaultValue="1.06" min="1.00" max="1.30" step=".01" className="uma-bonus-stam" />
+                                        <label>
+                                            Pow
+                                        </label>
+                                        <input type="number" defaultValue="1.06" min="1.00" max="1.30" step=".01" className="uma-bonus-pow" />
+                                        <label>
+                                            Guts
+                                        </label>
+                                        <input type="number" defaultValue="1.06" min="1.00" max="1.30" step=".01" className="uma-bonus-guts" />
+                                        <label>
+                                            Wit
+                                        </label>
+                                        <input type="number" defaultValue="1.06" min="1.00" max="1.30" step=".01" className="uma-bonus-wit" />
+                                    </div>
                             </div>
-                            <input type="text" className="searchbar" />
+
+                            <div className="stats-container">
+                                <div className="stats-container-header">
+                                    Target Stats
+                                </div>
+                                <div className="stats-target">
+                                    <label>
+                                        Spd
+                                    </label>
+                                    <input type="number" defaultValue="600" min="0" max="1200" className="searchbar" />
+                                    <label>
+                                        Stam
+                                    </label>
+                                    <input type="number" defaultValue="600" min="0" max="1200" className="searchbar" />
+                                    <label>
+                                        Pow
+                                    </label>
+                                    <input type="number" defaultValue="600" min="0" max="1200" className="searchbar" />
+                                    <label>
+                                        Guts
+                                    </label>
+                                    <input type="number" defaultValue="600" min="0" max="1200" className="searchbar" />
+                                    <label>
+                                        Wit
+                                    </label>
+                                    <input type="number" defaultValue="600" min="0" max="1200" className="searchbar" />
+                                </div>
+                            </div>
                         </div>
                         <Dropdown 
-                            customPrompt="What's your favorite Uma Musume?" 
+                            customPrompt="Uma Musume" 
                             dropdownOptions={staticUmaList}
                             onChangeHandler={optionSelected}
                         />
@@ -72,6 +133,25 @@ const UmaProject: FC = () => {
                         </button>
                     </form>
                 </FormProvider>
+                <div className="uma-cards-container">
+                    <div className="selected-deck">
+                        <div>
+                            Currently selected deck area.
+                        </div>
+                    </div>
+                    <div className="selected-card-type-container">
+                        <div className="selected-card-type-header">
+                            Select a Support Card To Add to your deck
+                        </div>
+                        <div className="selected-card-type">
+                            {cards.map((card) =>
+                                {
+                                    return <SupportCards imgUrl={`/cardImages/support_card_s_${card.id}.png`} alt={`${card.id}`} />
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
             </div>
             <Footer />
         </div>
